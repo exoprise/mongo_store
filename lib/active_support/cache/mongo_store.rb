91c85eafcc7930410a924e9c08e3545512bcebf0
bughit <bughit@users.noreply.github.com>
2017-02-01 18:47:04 -0500

    Collection#remove fixes

--- lib/active_support/cache/mongo_store.rb
+++ lib/active_support/cache/mongo_store.rb
@@ -20,7 +20,8 @@ module MongoStore
         ActiveSupport::Cache::Entry.new(doc['value']) if doc
       end
       def delete_entry(key, options=nil)
-        collection.delete_one({'_id' => key})
+        #collection.remove({'_id' => key})
+        collection.delete_many({'_id' => key})
       end
       def delete_matched(pattern, options=nil)
         options = merged_options(options)
@@ -94,12 +95,14 @@ module ActiveSupport
       # if the key itself never gets cached again.  It also means you can _reduce_ efficiency by running this
       # too often.)
       def clean_expired
-        collection.delete_all({'expires' => {'$lt' => Time.now}})
+        #collection.remove({'expires' => {'$lt' => Time.now}})
+        collection.delete_many({'expires' => {'$lt' => Time.now}})
       end
       
       # Wipes the whole cache.
       def clear
-        collection.delete_all
+        #collection.remove
+        collection.delete_many
       end
       
       private
